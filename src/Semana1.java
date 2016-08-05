cf5c49cddf1b38099a77822103fb43c0560c3b8c
hedmon lopes <hedmon lopes>
2016-08-05 12:28:52 -0600

    Probando Git

---
+++ src/Semana1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Semana1 {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.print("INGRESE UN NUMERO:  ");
+		int numero = s.nextInt();
+	    
+		
+		
+	}
+
+}
