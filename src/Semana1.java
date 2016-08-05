ce308dc17d66a8f4172a1aba56f680f33d60c98f
hedmon lopes <hedmon lopes>
2016-08-05 12:45:37 -0600

    Cambio

--- src/Semana1.java
+++ src/Semana1.java
@@ -5,7 +5,13 @@ public class Semana1 {
 		Scanner s = new Scanner(System.in);
 		System.out.print("INGRESE UN NUMERO:  ");
 		int numero = s.nextInt();
-	    
+		if(numero>10)
+		{
+	    System.out.print("Ingrese un numero: ");
+		}else
+		{
+			System.out.println("El numero es menir que 10");
+		}
 		
 		
 	}
